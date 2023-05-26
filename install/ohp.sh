a83c0c7425d3a5c7810b38828a90ef98970d1e7b
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-26 16:26:39 +0700

    Update ohp.sh

--- install/ohp.sh
+++ install/ohp.sh
@@ -1,5 +1,5 @@
 #wget https://github.com/${GitUser}/
-GitUser="PelangiSenja"
+GitUser="Kulanbagong1"
 #Direct Proxy Squid For OpenVPN TCP
 RED='\e[1;31m'
 GREEN='\e[0;32m'
@@ -21,7 +21,7 @@ Port_OHP='8585';
 
 #Installing ohp Server
 cd 
-wget -O /usr/local/bin/ohpd "https://raw.githubusercontent.com/${GitUser}/XRAY-MULTIPORT/main/ohpd"
+wget -O /usr/local/bin/ohpd "https://raw.githubusercontent.com/${GitUser}/sckiat/main/ohpd"
 chmod +x /usr/local/bin/ohpd
 
 #Buat Service Untuk OHP Dropbear
