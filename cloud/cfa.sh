80373bbea784b1d7696240e1ef7617c0e4ce918f
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-26 16:03:36 +0700

    Update cfa.sh

--- cloud/cfa.sh
+++ cloud/cfa.sh
@@ -1,5 +1,5 @@
 #wget https://github.com/${GitUser}/
-GitUser="PelangiSenja"
+GitUser="Kulanbagong1"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
@@ -7,7 +7,7 @@ clear
 # Valid Script
 VALIDITY () {
     today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/izinn/main/ip | grep $MYIP | awk '{print $4}')
     if [[ $today < $Exp1 ]]; then
     echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
     else
@@ -16,7 +16,7 @@ VALIDITY () {
     exit 0
 fi
 }
-IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/izinn/main/ip | awk '{print $5}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 VALIDITY
@@ -40,11 +40,11 @@ echo -e "### $domain $email" >> /root/mail.conf
 clear
 echo -e "\e[0;32mDONE\e[0m"
 echo -e "\e[0;33mYour ID Cloudflare\e[0m"
-echo -e "\e[1;34m===============================\e[0m"
+echo -e "\e[1;34m────────────────────────────────────────────\e[0m"
 echo -e "\e[0;36mDOMAIN         :\e[0m $domain"
 echo -e "\e[0;36mEmail          :\e[0m $email"
 echo -e "\e[0;36mApi Key        :\e[0m $key"
-echo -e "\e[1;34m===============================\e[0m"
+echo -e "\e[1;34m────────────────────────────────────────────\e[0m"
 echo -e "\e[0;32mNow you can use & add subdomain.\e[0m"
 echo -e "\e[0;32mGo to main menu and chosee Add Subdomain to you ID Cloudflare or Pointing IP\e[0m"
 echo -e "\e[0;32mto you ID Cloudflare\e[0m"
